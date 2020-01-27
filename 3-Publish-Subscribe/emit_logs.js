e3a877d8276e376863596f8dd7ee9ba869fc128e
kajaharish <kajaharish4hud@gmail.com>
2020-01-27 12:41:09 +0530

    added the publishing log's file

--- 3-Publish-Subscribe/emit_logs.js
+++ 3-Publish-Subscribe/emit_logs.js
@@ -4,14 +4,19 @@ amqp.connect('amqp://localhost',(error,connection)=>{
     if(error){
         throw error;
     }
-    connection.createChannel((error,channel)=>{
-        if(error){
-            throw error;
+    connection.createChannel((error1,channel)=>{
+        if(error1){
+            throw error1;
         }
         var exchange = "logs";
         var msg = process.argv.slice(2).join() || 'hello world';
 
         channel.assertExchange(exchange,'fanout',{durable:false});
         channel.publish(exchange,'',Buffer.from(msg));
-    })
-})
+        console.log("[x] msg sent %s",msg);
+    });
+    setTimeout(()=>{
+        connection.close();
+        process.exit(0);
+    },500);
+});
